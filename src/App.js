6fa3fa89b7c5d166b2b5368093926afcefa2050a
Stan <nedbailov375426@gmail.com>
2019-02-05 16:52:24 -0500

    Add xo config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, {lazy, Component, Suspense} from 'react'
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { Icon, List } from 'semantic-ui-react'
-import {importMDX} from 'mdx.macro'
+import React, { lazy, Component, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Icon, List } from 'semantic-ui-react';
+import { importMDX } from 'mdx.macro';
 import HighLightCode from './mark-down-utils/high-light-code.jsx';
 import logo from './logo.svg';
 import './App.css';
-
-
 
 const exampleCode = `
 import React from 'React';
@@ -19,62 +17,54 @@ const Foo = () => {
         )
     }
     `;
-
-class Mark extends Component {
-    render() {
-        const Content = lazy(() => importMDX('./content.mdx'))
-        return (
-            <div>
-                <Link to="/">Home</Link>
-                <Suspense fallback={<div>Loading...</div>}>
-                <Content />
-                </Suspense>
-                <HighLightCode
-                    code={exampleCode}
-                    language='js'
-                />
-            </div>
-        )
-    }
-}
+const Mark = () => {
+  const Content = lazy(() => {
+    return importMDX('./content.mdx');
+  });
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Content />
+      </Suspense>
+      <HighLightCode code={exampleCode} language="js" />
+    </div>
+  );
+};
 
 const Index = () => {
-    return (
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1>WPLDev</h1>
-        <List>
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>WPLDev</h1>
+      <List>
         <a href="https://www.twitter.com/venik_man">
-        <Icon name='twitter' />
+          <Icon name="twitter" />
         </a>
         <a href="https://www.github.com/venikman">
-        <Icon name='github' />
+          <Icon name="github" />
         </a>
-        </List>
+      </List>
 
-        <Link to="/about">
-        <Icon.Group size='big'>
-        <Icon loading size='large' name='spinner' />
-        <Icon name='server' size='tiny'/>
+      <Link to="/about">
+        <Icon.Group size="big">
+          <Icon loading size="large" name="spinner" />
+          <Icon name="server" size="tiny" />
         </Icon.Group>
-        </Link>
-        </header>
-        )
-    }
+      </Link>
+    </header>
+  );
+};
 
-
-
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={Index} />
-                    <Route path="/about" component={Mark} />
-                </Switch>
-            </Router>
-        );
-    }
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Index} />
+        <Route path="/about" component={Mark} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
--- src/mark-down-utils/high-light-code.jsx
+++ src/mark-down-utils/high-light-code.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import Highlight, { defaultProps } from "prism-react-renderer";
+import React from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
 
 const HighLightCode = ({ code, language }) => {
-    return (
-        <Highlight {...defaultProps} code={code} language={language}>
-            {({ className, style, tokens, getLineProps, getTokenProps }) => (
-                <pre className={className} style={style}>
-                {tokens.map((line, i) => (
-                    <div {...getLineProps({ line, key: i })}>
-                    {line.map((token, key) => (
-                        <span {...getTokenProps({ token, key })} />
-                    ))}
-                    </div>
-                ))}
-                </pre>
-            )}
-        </Highlight>
-    )
+  return (
+    <Highlight {...defaultProps} code={code} language={language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => {
+        return (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => {
+              return (
+                <div {...getLineProps({ line,
+                  key : i })}
+                >
+                  {line.map((token, key) => {
+                    return (
+                      <span {...getTokenProps({ token,
+                        key })}
+                      />
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </pre>
+        );
+      }}
+    </Highlight>
+  );
 };
 
 export default HighLightCode;
