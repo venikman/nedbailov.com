fa971baa8804e04a92eff9845698cb54e458f916
Stan <nedbailov375426@gmail.com>
2019-02-05 15:44:14 -0500

    Sed up highlighting for markdown code

--- src/App.js
+++ src/App.js
@@ -1,44 +1,68 @@
-import React, {Component} from 'react';
+import React, {lazy, Component, Suspense} from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Icon, List } from 'semantic-ui-react'
-import ReactMarkdown from 'react-markdown';
-
+import {importMDX} from 'mdx.macro'
+import HighLightCode from './mark-down-utils/high-light-code.jsx';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+const exampleCode = `
+import React from 'React';
+
+const Foo = () => {
+    return (
+        <p>
+            Quisquam doloremque incidunt facere qui sunt.
+        </p>
+        )
+    }
+    `;
+
+class Mark extends Component {
+    render() {
+        const Content = lazy(() => importMDX('./content.mdx'))
+        return (
+            <div>
+                <Link to="/">Home</Link>
+                <Suspense fallback={<div>Loading...</div>}>
+                <Content />
+                </Suspense>
+                <HighLightCode
+                    code={exampleCode}
+                    language='js'
+                />
+            </div>
+        )
+    }
+}
+
 const Index = () => {
     return (
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1>WPLDev</h1>
-                <List>
-                    <a href="https://www.twitter.com/venik_man">
-                        <Icon name='twitter' />
-                    </a>
-                    <a href="https://www.github.com/venikman">
-                        <Icon name='github' />
-                    </a>
-                </List>
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>WPLDev</h1>
+        <List>
+        <a href="https://www.twitter.com/venik_man">
+        <Icon name='twitter' />
+        </a>
+        <a href="https://www.github.com/venikman">
+        <Icon name='github' />
+        </a>
+        </List>
 
-                <Link to="/about">
-                    <Icon.Group size='big'>
-                        <Icon loading size='large' name='spinner' />
-                        <Icon name='server' size='tiny'/>
-                    </Icon.Group>
-                </Link>
-            </header>
+        <Link to="/about">
+        <Icon.Group size='big'>
+        <Icon loading size='large' name='spinner' />
+        <Icon name='server' size='tiny'/>
+        </Icon.Group>
+        </Link>
+        </header>
         )
     }
 
-    const Mark = () => {
-        const input = '# This is a header\n\nAnd this is a paragraph'
-        return (
-            <React.Fragment>
-                <Link to="/">Home</Link>
-                <ReactMarkdown source={input} />
-            </React.Fragment>
-        )
-}
+
 
 class App extends Component {
     render() {
