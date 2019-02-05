972d116adfe34848dfe1c2b5548f0f091735c5e1
Stan <nedbailov375426@gmail.com>
2019-02-04 21:27:11 -0500

    Set up markdown and rotes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React, {Component} from 'react';
-import { Icon } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Icon, List } from 'semantic-ui-react'
+import ReactMarkdown from 'react-markdown';
+
 import logo from './logo.svg';
 import './App.css';
+
+const Index = () => {
+    return (
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>WPLDev</h1>
+        <List>
+        <a href="https://www.twitter.com/venik_man">
+        <Icon name='twitter' />
+        </a>
+        <a href="https://www.github.com/venikman">
+        <Icon name='github' />
+        </a>
+        </List>
+        </header>
+        )
+    }
+
+    const Mark = () => {
+        const input = '# This is a header\n\nAnd this is a paragraph'
+        return (
+            <ReactMarkdown source={input} />
+        )
+}
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1>WPLDev</h1>
-                    <a href="https://www.twitter.com/venik_man">
-                        <Icon name='twitter' />
-                    </a>
-                </header>
-            </div>
+            <Router>
+                <React.Fragment>
+
+                    <Route path="/" exact component={Index} />
+                    <Route path="/about/" component={Mark} />
+                </React.Fragment>
+            </Router>
         );
     }
 }
